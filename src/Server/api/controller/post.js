25e90754ccaaa09877d1417ecf6537255e579333
alexeyMohnatkin <dizzymohnatkin@gmail.com>
2018-03-17 22:07:33 +0300

    fix action handler

--- src/Server/api/controller/post.js
+++ src/Server/api/controller/post.js
@@ -7,47 +7,39 @@ export default (req, res) => {
 	switch (action) {
 		case 'closeWindow':
 			EventBus.emit('CLOSE_WINDOW');
-			res.status(200).send('ok');
 			break;
 		case 'lowTmp':
 			EventBus.emit(EVENTS.TEMPERATURE.LOW_LEVEL);
-			res.status(200).send('ok');
 			break;
 		case 'highTmp':
 			EventBus.emit(EVENTS.TEMPERATURE.HIGH_LEVEL);
-			res.status(200).send('ok');
 			break;
 		case 'normalTmp':
 			EventBus.emit(EVENTS.TEMPERATURE.NORMAL_LEVEL);
-			res.status(200).send('ok');
 			break;
 		case 'lowCO2':
 			EventBus.emit(EVENTS.CO2.LOW_LEVEL);
-			res.status(200).send('ok');
 			break;
 		case 'highCO2':
 			EventBus.emit(EVENTS.CO2.HIGH_LEVEL);
-			res.status(200).send('ok');
 			break;
 		case 'normalCO2':
 			EventBus.emit(EVENTS.CO2.NORMAL_LEVEL);
-			res.status(200).send('ok');
 			break;
 		case 'lowLight':
 			EventBus.emit(EVENTS.LIGHT.LOW_LEVEL);
-			res.status(200).send('ok');
 			break;
 		case 'highLight':
 			EventBus.emit(EVENTS.LIGHT.HIGH_LEVEL);
-			res.status(200).send('ok');
 			break;
 		case 'normalLight':
 			EventBus.emit(EVENTS.LIGHT.NORMAL_LEVEL);
-			res.status(200).send('ok');
 			break;
 		default:
 			res.status(400).send('no such action');
 			break;
 	}
+
+	res.status(200).send('ok');
 };
 
