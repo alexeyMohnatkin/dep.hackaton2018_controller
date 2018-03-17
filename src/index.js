c236eafd3ab9893113ba916c15c4d26a2217c29e
alexeyMohnatkin <dizzymohnatkin@gmail.com>
2018-03-17 13:23:07 +0300

    create mock

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
-import five from 'johnny-five';
+// import five from 'johnny-five';
+import fetch from 'node-fetch';
 
 
-console.log('initializing...');
-const board = new five.Board();
+// console.log('initializing...');
+// const board = new five.Board();
 
-board.on('ready', () => {
-	console.log('ready');
-});
+// board.on('ready', () => {
+// 	console.log('ready');
+// });
+
+const getRandomValue = (min, max) => parseInt((Math.random() * (max - min)) + min, 10);
+const generateData = () => {
+	const tmp = getRandomValue(20, 25);
+	const co2 = getRandomValue(600, 650);
+	const light = getRandomValue(300, 320);
+
+	return {
+		tmp,
+		co2,
+		light,
+	};
+};
+
+const senData = async () => {
+	const data = generateData();
+	try {
+		const res = await fetch('http://10.66.170.54:8000/controller', {
+			method: 'post',
+			body: data,
+		});
+		console.dir(data);
+		console.log(res.ok);
+		console.log(`server says: ${await res.text()}`);
+	} catch (error) {
+		console.error(error);
+	}
+};
+setInterval(senData, 10000);
