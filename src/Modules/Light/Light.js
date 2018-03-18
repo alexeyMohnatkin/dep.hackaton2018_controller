2839c0629620d4990fc6a77be185154a5412c6ba
alexeyMohnatkin <dizzymohnatkin@gmail.com>
2018-03-18 09:22:46 +0300

    normalize light value

--- src/Modules/Light/Light.js
+++ src/Modules/Light/Light.js
@@ -18,7 +18,16 @@ export default class Light {
 
 	init() {
 		this.sensor.on('data', (value) => {
-			EventBus.emit(EVENTS.VALUE_CHANGED, value);
+			EventBus.emit(EVENTS.VALUE_CHANGED, this.normalizeValue(value));
 		});
+	}
+
+	normalizeValue(value) {
+		const Vin = 5;
+		const Vout = value * (Vin / 1024);
+		const lx = ((2500 / Vout) - 500) / 10;
+		const CORRECTION = 3.3;
+
+		return parseInt(lx * CORRECTION, 10);
 	}
 }
